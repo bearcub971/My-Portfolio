10943778b1de64a60ad181aa344cb2add4b44659
Yohann Cyrille <971cyohann@gmail.com>
2025-10-09 17:42:32 +0200

    responsive

---
+++ js/burgerMenu.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const menu = document.querySelector(".menu");
+  
+    burger.addEventListener("click", () => {
+      burger.classList.toggle("active");
+      menu.classList.toggle("active");
+    });
+  
+    // Ferme le menu quand on clique sur un lien
+    document.querySelectorAll(".menu a").forEach(link => {
+      link.addEventListener("click", () => {
+        burger.classList.remove("active");
+        menu.classList.remove("active");
+      });
+    });
+  });
+  
